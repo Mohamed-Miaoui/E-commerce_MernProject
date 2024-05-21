c0af60c4f105063e30c902d6d21391fd3492b145
Mohamed-Mi20 <miaoui.mohamed12@gmail.com>
2024-05-21 12:01:41 +0100

    responsive navbar

--- frontend/src/Components/NavBar/NavBar.jsx
+++ frontend/src/Components/NavBar/NavBar.jsx
@@ -1,22 +1,33 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useRef, useState } from "react";
 import "./NavBar.css";
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
 import { Link } from "react-router-dom";
 import { ShopContext } from "../../Context/ShopContext";
+import nav_dropdown from "../Assets/nav-dropdown.png";
 
 //prettier-ignore
 export const NavBar = () => {
   const [menu, setMenu] = useState("shop");
   const {getTotalCartItems} =useContext(ShopContext)
 
+  // change nav bar to drop-down icon logic when size is small
+  const menuRef = useRef()
+  const dropdown_toggle = (e) => {
+      menuRef.current.classList.toggle('nav-menuvisible')
+      e.target.classList.toggle('open'); //icon rotation change from css when clicked on
+    }
+
+
   return (
     <div className="navbar">
       <div className="nav-logo">
         <img className="logo" src={logo}></img>
-        <p>SHOP</p>
+        <p>Gamix</p>
       </div>
-      <ul className="nav-menu">
+      <img className="nav-dropdown" onClick={dropdown_toggle} src={nav_dropdown} alt="" />
+      {/* when clicking on icom img nav-menuvisible will be added to ul along with nav-menu  */}
+      <ul ref={menuRef} className="nav-menu">
         <li onClick={() => {setMenu("shop")}}> <Link style={{textDecoration:'none'}} to='/'> Shop </Link> {menu ==="shop" ? <hr/>:<></>} </li>
         <li onClick={() => {setMenu("console")}}> <Link style={{textDecoration:'none'}} to='/console'> Console & accessories </Link> {menu ==="console" ? <hr/>:<></>}</li>
         <li onClick={() => {setMenu("game")}}> <Link style={{textDecoration:'none'}} to='/game'> Games </Link> {menu ==="game" ? <hr/>:<></>}</li>
--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -2,6 +2,7 @@ import React, { createContext, useState } from "react";
 import all_product from "../../src/Components/Assets/all_product";
 export const ShopContext = createContext(null);
 
+//intialize the default empty cart [key =id,value=quantity]
 const getDefaultCart = () => {
   let cart = {};
   for (let index = 0; index < all_product.length + 1; index++) {
@@ -14,6 +15,7 @@ const ShopContextPovider = (props) => {
   const [cartItems, setCartItems] = useState(getDefaultCart());
 
   const addToCart = (itemId) => {
+    //key = id and then acces its value and add one to it
     setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }));
     console.log(cartItems);
   };
@@ -24,6 +26,7 @@ const ShopContextPovider = (props) => {
     let totalAmount = 0;
     for (const item in cartItems) {
       console.log(cartItems);
+      //get all items in cart (cartItems[item]= nb of a specifique item in cart)
       if (cartItems[item] > 0) {
         let itemInfo = all_product.find(
           (product) => product.id === Number(item)
@@ -35,6 +38,7 @@ const ShopContextPovider = (props) => {
     return totalAmount;
   };
 
+  //nb of items in cart for NavBar
   const getTotalCartItems = () => {
     let totalItem = 0;
     for (const item in cartItems) {
